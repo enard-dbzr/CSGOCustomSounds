b0919dd981af6ae3d3ba2934ead5d2da175ce709
Сергей <serg.miass340@gmail.com>
2020-01-07 23:44:17 +0500

    frst stab ver

---
+++ CSGOCustomSounds/CSSounds/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace CSSounds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.trackBar_volume = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar_feezetime = new System.Windows.Forms.TrackBar();
+            this.trackBar_kill = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar_death = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar_mvp = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_volume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_feezetime)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_kill)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_death)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_mvp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(539, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(423, 499);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 514);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(983, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // trackBar_volume
+            // 
+            this.trackBar_volume.Location = new System.Drawing.Point(136, 24);
+            this.trackBar_volume.Maximum = 100;
+            this.trackBar_volume.Name = "trackBar_volume";
+            this.trackBar_volume.Size = new System.Drawing.Size(181, 45);
+            this.trackBar_volume.TabIndex = 2;
+            this.trackBar_volume.TickFrequency = 10;
+            this.trackBar_volume.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar_volume.Value = 100;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(29, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Main volume:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(29, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Freezetime:";
+            // 
+            // trackBar_feezetime
+            // 
+            this.trackBar_feezetime.Location = new System.Drawing.Point(136, 100);
+            this.trackBar_feezetime.Maximum = 100;
+            this.trackBar_feezetime.Name = "trackBar_feezetime";
+            this.trackBar_feezetime.Size = new System.Drawing.Size(181, 45);
+            this.trackBar_feezetime.TabIndex = 4;
+            this.trackBar_feezetime.TickFrequency = 10;
+            this.trackBar_feezetime.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar_feezetime.Value = 100;
+            // 
+            // trackBar_kill
+            // 
+            this.trackBar_kill.Location = new System.Drawing.Point(136, 152);
+            this.trackBar_kill.Maximum = 100;
+            this.trackBar_kill.Name = "trackBar_kill";
+            this.trackBar_kill.Size = new System.Drawing.Size(181, 45);
+            this.trackBar_kill.TabIndex = 4;
+            this.trackBar_kill.TickFrequency = 10;
+            this.trackBar_kill.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar_kill.Value = 100;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(29, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kill:";
+            // 
+            // trackBar_death
+            // 
+            this.trackBar_death.Location = new System.Drawing.Point(136, 202);
+            this.trackBar_death.Maximum = 100;
+            this.trackBar_death.Name = "trackBar_death";
+            this.trackBar_death.Size = new System.Drawing.Size(181, 45);
+            this.trackBar_death.TabIndex = 4;
+            this.trackBar_death.TickFrequency = 10;
+            this.trackBar_death.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar_death.Value = 100;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(29, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Death:";
+            // 
+            // trackBar_mvp
+            // 
+            this.trackBar_mvp.Location = new System.Drawing.Point(136, 256);
+            this.trackBar_mvp.Maximum = 100;
+            this.trackBar_mvp.Name = "trackBar_mvp";
+            this.trackBar_mvp.Size = new System.Drawing.Size(181, 45);
+            this.trackBar_mvp.TabIndex = 4;
+            this.trackBar_mvp.TickFrequency = 10;
+            this.trackBar_mvp.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar_mvp.Value = 100;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(29, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "MVP:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(136, 309);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(181, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.Value = 100;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(29, 319);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Won round:";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(136, 363);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(181, 45);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.TickFrequency = 10;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar2.Value = 100;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(29, 373);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 20);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Lost round:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 536);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar_mvp);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar_death);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar_kill);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar_feezetime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar_volume);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "CSGOCustomSounds";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_volume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_feezetime)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_kill)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_death)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_mvp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TrackBar trackBar_volume;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar_feezetime;
+        private System.Windows.Forms.TrackBar trackBar_kill;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar_death;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar_mvp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ CSGOCustomSounds/CSSounds/Form1.cs
@@ -0,0 +1,146 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSSounds
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Checker()
+        {
+            double mainVolume = 1, curVolume = 1;
+            string providerSteamID, playerSteamID,
+                phase, prevPhase = "",
+                team = "";
+            int kills, prevKills = 9999,
+                deaths, prevDeaths = 9999,
+                mvps, prevMvps = 9999;
+            bool alredyWon = false;
+
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8888/");
+            listener.Start();
+            WMPLib.WindowsMediaPlayer WMP = new WMPLib.WindowsMediaPlayer();
+            WMP.settings.volume = 9;
+            Log("Ожидание подключений...");
+
+            while (true)
+            {
+                Invoke(new Action(() => mainVolume = trackBar_volume.Value / 100.0));
+
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+
+                string responseString = "Good";
+
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                string str;
+                using (var reader = new StreamReader(request.InputStream))
+                    str = reader.ReadToEnd();
+                output.Write(buffer, 0, buffer.Length);
+                JObject json = JObject.Parse(str);
+
+                providerSteamID = json["provider"]["steamid"].ToString();
+                playerSteamID = json["player"]["steamid"].ToString();
+                team = json["player"]["team"].ToString();
+                phase = json["round"]["phase"].ToString();
+                kills = Convert.ToInt32(json["player"]["match_stats"]["kills"].ToString());
+                deaths = Convert.ToInt32(json["player"]["match_stats"]["deaths"].ToString());
+                mvps = Convert.ToInt32(json["player"]["match_stats"]["mvps"].ToString());
+
+
+                if(phase == "freezetime" && prevPhase != phase)
+                {
+                    WMP.URL = "startround.mp3";
+                    Invoke(new Action(() => curVolume = trackBar_feezetime.Value));
+                    WMP.settings.volume = Convert.ToInt32(curVolume * mainVolume);
+                    WMP.controls.play();
+                }
+                if (kills > prevKills && providerSteamID == playerSteamID)
+                {
+                    WMP.URL = "kill.mp3";
+                    Invoke(new Action(() => curVolume = trackBar_kill.Value));
+                    WMP.settings.volume = Convert.ToInt32(curVolume * mainVolume);
+                    WMP.controls.play();
+                }
+                if (deaths > prevDeaths && providerSteamID == playerSteamID)
+                {
+                    WMP.URL = "die.mp3";
+                    Invoke(new Action(() => curVolume = trackBar_death.Value));
+                    WMP.settings.volume = Convert.ToInt32(curVolume * mainVolume);
+                    WMP.controls.play();
+                }
+                if(phase == "over" && !alredyWon)
+                {
+                    alredyWon = true;
+                    if (json["round"]["win_team"].ToString() == team)
+                    {
+                        WMP.URL = "wonround.mp3";
+                        Invoke(new Action(() => curVolume = trackBar_mvp.Value));
+                        WMP.settings.volume = Convert.ToInt32(curVolume * mainVolume);
+                        WMP.controls.play();
+                    }
+                    else
+                    {
+                        WMP.URL = "lostround.mp3";
+                        Invoke(new Action(() => curVolume = trackBar_mvp.Value));
+                        WMP.settings.volume = Convert.ToInt32(curVolume * mainVolume);
+                        WMP.controls.play();
+                    }  
+                }
+                else if(phase != "over")
+                {
+                    alredyWon = false;
+                }
+                if (mvps > prevMvps && providerSteamID == playerSteamID)
+                {
+                    WMP.URL = "mvp.mp3";
+                    Invoke(new Action(() => curVolume = trackBar_mvp.Value));
+                    WMP.settings.volume = Convert.ToInt32(curVolume * mainVolume);
+                    WMP.controls.play();
+                }
+
+                Invoke(new Action(() => richTextBox1.AppendText(str + ",\n")));
+                Invoke(new Action(() => richTextBox1.ScrollToCaret()));
+
+                prevPhase = phase;
+                if(providerSteamID == playerSteamID)
+                {
+                    prevKills = kills;
+                    prevDeaths = deaths;
+                    prevMvps = mvps;
+                }
+                output.Close();
+            }
+
+        }  
+        void Log(string str)
+        {
+            toolStripStatusLabel1.Text = str;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread th = new Thread(Checker);
+            th.Start();
+        }
+    }
+}
